ae401bf09983fb5097d4e0c74d694a6738d3a891
Anakinvl <anakvanw@student.arteveldehs.be>
2024-09-30 13:19:55 +0200

    Big fixes

--- scripts/loadHeader.js
+++ scripts/loadHeader.js
@@ -3,21 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
-            setActiveNavItem();
+
+            // Highlight the current page link
+            const currentPage = window.location.pathname.split('/').pop();
+            const navLinks = document.querySelectorAll('nav a');
+            navLinks.forEach(link => {
+                if (link.getAttribute('href') === currentPage) {
+                    link.classList.add('active');
+                }
+            });
         });
 });
-
-function setActiveNavItem() {
-    const currentPath = window.location.pathname;
-    console.log('Current Path:', currentPath);
-    const navLinks = document.querySelectorAll('nav a');
-
-    navLinks.forEach(link => {
-        console.log('Link Href:', link.getAttribute('href'));
-        if (link.getAttribute('href') === currentPath.slice(1)) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
-    });
-}
